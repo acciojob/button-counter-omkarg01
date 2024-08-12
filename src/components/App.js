775ded8747917aae47614ba6ada83d290add3707
Omkar Gujja <67428719+omkarg01@users.noreply.github.com>
2024-08-12 12:28:25 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <div>
       <p> Button clicked {count} times. </p>
-      <button onClick={() => setCount(count + 1)}> Click Me </button>
+      <button onClick={() => setCount(count + 1)}> Click me </button>
     </div>
   )
 }
